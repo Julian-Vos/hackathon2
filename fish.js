79651486722eb964ab8fa51e82973cd404b6f2de
Julian Vos <julianvosje@gmail.com>
2022-12-31 18:22:59 +0100

    Added a marquee for selecting fish

---
+++ fish.js
@@ -0,0 +1,29 @@
+class Fish {
+    constructor(x, y) {
+        this.displayObject = new PIXI.Graphics()
+        this.displayObject.beginFill(0xffffff).drawRect(-50, -50, 100, 100)
+        this.displayObject.position.x = x
+        this.displayObject.position.y = y
+
+        this.marqueed = false
+        this.selected = false
+
+        this.updateTint()
+    }
+
+    setMarqueed(value) {
+        this.marqueed = value
+
+        this.updateTint()
+    }
+
+    setSelected(value) {
+        this.selected = value
+
+        this.updateTint()
+    }
+
+    updateTint() {
+        this.displayObject.tint = this.marqueed || this.selected ? 0x0000ff : 0xff0000
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,88 @@
 const app = new PIXI.Application({
     autoDensity: true,
+    backgroundColor: 0xd4f1f9,
     resizeTo: window,
     resolution: devicePixelRatio,
-    useContextAlpha: false,
 })
 
 app.view.style.display = 'block'
 
 document.body.appendChild(app.view)
+
+const objects = []
+
+for (let i = 0; i < 4; i++) {
+    const fish = new Fish(60 + i * 120, 60 + i * 120)
+
+    fish.displayObject.interactive = true
+    fish.displayObject.cursor = 'pointer'
+
+    app.stage.addChild(fish.displayObject)
+
+    objects.push(fish)
+}
+
+const marquee = new PIXI.Graphics()
+
+app.stage.addChild(marquee)
+
+document.addEventListener('mousedown', (event) => {
+    marquee.position.x = event.x
+    marquee.position.y = event.y
+
+    app.stage.interactiveChildren = false
+
+    document.addEventListener('mousemove', onMouseMove)
+    document.addEventListener('mouseup', onMouseUp)
+    document.addEventListener('keydown', onKeyDown)
+})
+
+function onMouseMove(event) {
+    marquee.clear()
+    marquee.lineStyle(3).drawRect(
+        Math.min(0, event.x - marquee.x),
+        Math.min(0, event.y - marquee.y),
+        Math.abs(event.x - marquee.x),
+        Math.abs(event.y - marquee.y)
+    )
+
+    for (const object of objects) {
+        object.setMarqueed(object.displayObject.x - 50 < Math.max(event.x, marquee.x) &&
+                           object.displayObject.y - 50 < Math.max(event.y, marquee.y) &&
+                           object.displayObject.x + 50 > Math.min(marquee.x, event.x) &&
+                           object.displayObject.y + 50 > Math.min(marquee.y, event.y))
+    }
+}
+
+function onMouseUp(event) {
+    if (event.x === marquee.position.x && event.y === marquee.position.y) {
+        onMouseMove(event)
+    }
+
+    for (const object of objects) {
+        object.setSelected(object.marqueed)
+        object.setMarqueed(false)
+    }
+
+    hideMarquee()
+}
+
+function hideMarquee() {
+    marquee.clear()
+
+    app.stage.interactiveChildren = true
+
+    document.removeEventListener('mousemove', onMouseMove)
+    document.removeEventListener('mouseup', onMouseUp)
+    document.removeEventListener('keydown', onKeyDown)
+}
+
+function onKeyDown(event) {
+    if (event.key === 'Escape') {
+        for (const object of objects) {
+            object.setMarqueed(false)
+        }
+
+        hideMarquee()
+    }
+}
